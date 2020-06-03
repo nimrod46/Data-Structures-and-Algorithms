54432b3375fc59e540ca9493614c5e08ff2b794d
nimrod46 <nimrodma99@gmail.com>
2020-06-03 13:15:40 +0300

    Added hash table "first recurring character" solution

---
+++ src/data_structures/hash_table/FirstRecurringCharacter.java
@@ -0,0 +1,47 @@
+package data_structures.hash_table;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+public class FirstRecurringCharacter {
+
+  public static Object getFirstRecurringObject(Object[] inputArray) {
+    for (int i = 0; i < inputArray.length; i++) {
+      for (int j = i + 1; j < inputArray.length; j++) {
+        if(inputArray[i].equals(inputArray[j])) {
+          return inputArray[i];
+        }
+      }
+    }
+    return null;
+  }
+
+  public static Object getFirstRecurringObjectBetter(Object[] inputArray) {
+    HashSet<Object> map = new HashSet<>();
+    for (Object obj : inputArray) {
+      if(map.contains(obj)) {
+        return obj;
+      } else {
+        map.add(obj);
+      }
+    }
+    return null;
+  }
+
+  public static Object getFirstRecurringObjectBetter2(Object[] inputArray) {
+    HashSet<Object> map = new HashSet<>(Arrays.asList(inputArray)); //Filling the hash table before checking if item exists
+    for (Object obj : inputArray) {
+      if (map.contains(obj)) {
+        return obj;
+      }
+    }
+    return null;
+  }
+
+  public static void main(String[] args) {
+    Object[] arr = {1,5,8,5,1,8,8,7,4,4,10};
+    System.out.println(getFirstRecurringObjectBetter(arr));
+    System.out.println(getFirstRecurringObjectBetter2(arr));
+    System.out.println(getFirstRecurringObject(arr));
+  }
+}

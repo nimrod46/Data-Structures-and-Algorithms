24281349b74a3f0cf7c9cc79592280c7b669c01e
nimrod46 <nimrodma99@gmail.com>
2020-06-03 13:12:52 +0300

    Added hash table solution

---
+++ src/data_structures/hash_table/MyNode.java
@@ -0,0 +1,28 @@
+package data_structures.hash_table;
+
+public class MyNode {
+    private String key;
+    private int value;
+
+
+  public MyNode(String key, int value) {
+    this.key = key;
+    this.value = value;
+  }
+
+  public String getKey() {
+    return key;
+  }
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+
+  public int getValue() {
+    return value;
+  }
+
+  public void setValue(int value) {
+    this.value = value;
+  }
+}

7172aac32be267b014d5773118a2b7f69bc08796
nimrod46 <nimrodma99@gmail.com>
2020-06-19 14:30:34 +0300

    Fixed PR indent format

--- src/data_structures/hash_tables/MyHashTable.java
+++ src/data_structures/hash_tables/MyHashTable.java
@@ -10,81 +10,81 @@ import java.util.List;
  */
 public class MyHashTable {
 
-    private static class MyNodes extends ArrayList<MyNode> {
+  private static class MyNodes extends ArrayList<MyNode> {
+  }
+
+  private final int length;
+  private MyNodes[] data;
+
+  public MyHashTable(int size) {
+    length = size;
+    data = new MyNodes[length];
+  }
+
+  public void set(String key, int value) {
+    if (data[hash(key)] == null) {
+      data[hash(key)] = new MyNodes();
     }
+    data[hash(key)].add(new MyNode(key, value));
+  }
 
-    private final int length;
-    private MyNodes[] data;
-
-    public MyHashTable(int size) {
-        length = size;
-        data = new MyNodes[length];
+  public int get(String key) {
+    if (data[hash(key)] == null) {
+      return 0;
     }
+    for (MyNode node : data[hash(key)]) {
+      if (node.getKey().equals(key)) {
+        return node.getValue();
+      }
+    }
+    return 0;
+  }
 
-    public void set(String key, int value) {
-        if (data[hash(key)] == null) {
-            data[hash(key)] = new MyNodes();
+  public List<String> keys() {
+    List<String> keys = new ArrayList<>();
+    for (int i = 0; i < length; i++) {
+      if (data[i] != null) {
+        for (int j = 0; j < data[i].size(); j++) {
+          keys.add(data[i].get(j).getKey());
         }
-        data[hash(key)].add(new MyNode(key, value));
+      }
     }
+    return keys;
+  }
 
-    public int get(String key) {
-        if (data[hash(key)] == null) {
-            return 0;
+  public List<String> keyNoCollision() {
+    List<String> keys = new ArrayList<>();
+    for (MyNodes myNodes : data) {
+      if (myNodes != null && myNodes.size() != 0) {
+        if (data.length > 1) {
+          for (MyNode myNode : myNodes) {
+            keys.add(myNode.getKey());
+          }
+        } else {
+          keys.add(myNodes.get(0).getKey());
         }
-        for (MyNode node : data[hash(key)]) {
-            if (node.getKey().equals(key)) {
-                return node.getValue();
-            }
-        }
-        return 0;
+      }
     }
+    return keys;
+  }
 
-    public List<String> keys() {
-        List<String> keys = new ArrayList<>();
-        for (int i = 0; i < length; i++) {
-            if (data[i] != null) {
-                for (int j = 0; j < data[i].size(); j++) {
-                    keys.add(data[i].get(j).getKey());
-                }
-            }
-        }
-        return keys;
+  private int hash(String key) {
+    int hash = 0;
+    for (int i = 0; i < key.length(); i++) {
+      hash = (hash + key.charAt(i) * i) % length;
     }
+    return hash;
+  }
 
-    public List<String> keyNoCollision() {
-        List<String> keys = new ArrayList<>();
-        for (MyNodes myNodes : data) {
-            if (myNodes != null && myNodes.size() != 0) {
-                if (data.length > 1) {
-                    for (MyNode myNode : myNodes) {
-                        keys.add(myNode.getKey());
-                    }
-                } else {
-                    keys.add(myNodes.get(0).getKey());
-                }
-            }
-        }
-        return keys;
-    }
-
-    private int hash(String key) {
-        int hash = 0;
-        for (int i = 0; i < key.length(); i++) {
-            hash = (hash + key.charAt(i) * i) % length;
-        }
-        return hash;
-    }
-
-    public static void main(String[] args) {
-        MyHashTable table = new MyHashTable(50);
-        table.set("ta", 1000);
-        table.set("tu", 589);
-        table.set("e", 10);
-        System.out.println(table.keys().size());
-        System.out.println(table.keyNoCollision().size());
-        System.out.println(table.get("ta"));
-        System.out.println(table.get("tu"));
-        System.out.println(table.get("e"));
-    }
+  public static void main(String[] args) {
+    MyHashTable table = new MyHashTable(50);
+    table.set("ta", 1000);
+    table.set("tu", 589);
+    table.set("e", 10);
+    System.out.println(table.keys().size());
+    System.out.println(table.keyNoCollision().size());
+    System.out.println(table.get("ta"));
+    System.out.println(table.get("tu"));
+    System.out.println(table.get("e"));
+  }
 }
